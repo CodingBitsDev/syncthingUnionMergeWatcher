a904f2d47865f7969a309f724f70747c5fd0fa8e
CodingBitsDev <github@hoyits.me>
2023-05-07 14:24:39 +0200

    ensure merging is only done when the folder is a syncthing folder

--- watch.sh
+++ watch.sh
@@ -1,11 +1,18 @@
+
 #!/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-##Run initially
 shopt -s globstar
 cd $1
+
+#wait until folder is a syncthing folder
 path=$(pwd)
+until [ -d "$path/.stfolder" ]; do
+    echo "Not a Syncthing folder. .stfolder is missing"
+    sleep 5;
+done
  
+##Run initially
 for i in ./**/*
 do
     filePath="$path/${i//.\//}"
